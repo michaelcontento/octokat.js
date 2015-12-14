5fa8ea4ca4f069d06fc200374bde3fa9d83ffd3e
Philip Schatz <phil@cnx.org>
2015-12-13 21:02:05 -0500

    support & in URL templates

--- dist/octokat.js
+++ dist/octokat.js
@@ -12665,7 +12665,7 @@ toQueryString = require('./helper-querystring');
 deprecate = require('./deprecate');
 
 module.exports = function() {
-  var args, fieldName, i, j, len, m, match, optionalNames, optionalParams, param, templateParams, url;
+  var args, fieldName, fieldValue, i, j, k, len, len1, m, match, optionalNames, optionalParams, param, templateParams, url;
   url = arguments[0], args = 2 <= arguments.length ? slice.call(arguments, 1) : [];
   if (args.length === 0) {
     templateParams = {};
@@ -12682,8 +12682,19 @@ module.exports = function() {
     switch (match[1]) {
       case '/':
         fieldName = match.slice(2, match.length - 1);
-        if (templateParams[fieldName]) {
-          param = "/" + templateParams[fieldName];
+        fieldValue = templateParams[fieldName];
+        if (fieldValue) {
+          if (/\//.test(fieldValue)) {
+            throw new Error("Octokat Error: this field must not contain slashes: " + fieldName);
+          }
+          param = "/" + fieldValue;
+        }
+        break;
+      case '+':
+        fieldName = match.slice(2, match.length - 1);
+        fieldValue = templateParams[fieldName];
+        if (fieldValue) {
+          param = fieldValue;
         }
         break;
       case '?':
@@ -12695,12 +12706,21 @@ module.exports = function() {
         }
         param = toQueryString(optionalParams);
         break;
+      case '&':
+        optionalNames = match.slice(2, -1).split(',');
+        optionalParams = {};
+        for (k = 0, len1 = optionalNames.length; k < len1; k++) {
+          fieldName = optionalNames[k];
+          optionalParams[fieldName] = templateParams[fieldName];
+        }
+        param = toQueryString(optionalParams, true);
+        break;
       default:
         fieldName = match.slice(1, match.length - 1);
         if (templateParams[fieldName]) {
           param = templateParams[fieldName];
         } else {
-          throw new Error("Octokat Error: param " + fieldName + " is required");
+          throw new Error("Octokat Error: Required parameter is missing: " + fieldName);
         }
     }
     url = url.replace(match, param);
@@ -12849,7 +12869,7 @@ module.exports = {
 },{}],8:[function(require,module,exports){
 var toQueryString;
 
-toQueryString = function(options) {
+toQueryString = function(options, omitQuestionMark) {
   var key, params, ref, value;
   if (!options || options === {}) {
     return '';
@@ -12863,7 +12883,11 @@ toQueryString = function(options) {
     }
   }
   if (params.length) {
-    return "?" + (params.join('&'));
+    if (omitQuestionMark) {
+      return "&" + (params.join('&'));
+    } else {
+      return "?" + (params.join('&'));
+    }
   } else {
     return '';
   }
